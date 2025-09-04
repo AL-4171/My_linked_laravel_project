5fb63594da8f0b8933fa7e14fcf6648a74441aa4
Kaber Abd Elhamid Shawky Elredeny <shawky10117@gmail.com>
2025-09-04 19:18:22 +0300

    2025_08_26_140704_create_tags_table.php

--- database/migrations/2025_08_26_140704_create_tags_table.php
+++ database/migrations/2025_08_26_140704_create_tags_table.php
@@ -14,10 +14,9 @@ class CreateTagsTable extends Migration
     public function up()
 {
     Schema::create('tags', function (Blueprint $table) {
-        $table->bigIncrements('id');
-        $table->string('name');
-        $table->string('slug')->unique();
-        $table->timestamps();
+        $table->bigIncrements('TagID');
+            $table->string('Name', 50);
+            $table->timestamps();
     });
 }
 
